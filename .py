3768542b76bd55b87831ad9b04fabfd397dfcddb
brianfte <brianfte8@gmail.com>
2020-10-20 23:03:44 -0500

    second commit

---
+++ .py
@@ -0,0 +1,8 @@
+
+#suma con python en Linux
+
+a = int(input('ingrese un numero: ')) 
+b = int(input('ingrese un numero: '))
+suma = a + b
+
+print('el resultado es: '+str(suma)) 
---
+++ suma.py
@@ -0,0 +1,7 @@
+
+#suma con python en BackBoxLinux
+ a =(int(input('ingrese un numero: ')
+ b =(int(input('ingrese otro numero: ')
+ suma = a + b
+
+print('el resultado es: '+str(suma))
